435a610f236a5a2eac7b6eb5770579ba0aabdea0
TenzinYafel <yarphel2003@gmail.com>
2022-08-30 14:56:11 -0400

    IN CLASS NOTES

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,12 @@
 // By convention, class names are capitalized.
 public class HelloWorld {
+
     public static void main(String [] args){
         System.out.println("Hello World! Tenzin Yarphel's first Java Code.");
+
+        int x = 5;
+        boolean z = true;
+        double pi = 3.14159;
 
     }
 }
