fb67b0d410795078b3e689029606e6d8dc22dc80
TenzinYafel <yarphel2003@gmail.com>
2022-08-30 15:15:05 -0400

    IN CLASS NOTES

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,20 @@
 // By convention, class names are capitalized.
 public class HelloWorld {
 
+    /*
+        This is a multiline block comment.
+        Such as this one "Another Line"
+     */
     public static void main(String [] args){
         System.out.println("Hello World! Tenzin Yarphel's first Java Code.");
 
         int x = 5;
         boolean z = true;
         double pi = 3.14159;
+        String name = "CS219 Techniques of Computer Science";
+
+        double avagadros = 6.022140857e23; // Scientific Notation
+        double electron_mass = 9.1093837015e-31;
 
     }
 }
