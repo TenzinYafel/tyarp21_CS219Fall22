992622cf957ee88ffb4c638465b2c3ff623e34ea
TenzinYafel <yarphel2003@gmail.com>
2022-08-30 15:48:37 -0400

    using functions and calling functions

--- src/Investment.java
+++ src/Investment.java
@@ -1,2 +1,10 @@
 public class Investment {
+
+    public static double investment(double c, double r, int t, int n){
+        return c*Math.pow(1+r/n, t*n);
+        // return sends the value back to the function and can be used later.
+    }
+    public static void main(String [] args){
+        System.out.println(investment(1.0, 1.0 , 1 , (int)1e9));
+    }
 }
