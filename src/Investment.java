b62f403168ce530c5a15d066708441c53330364d
TenzinYafel <yarphel2003@gmail.com>
2022-09-01 15:40:55 -0400

    Class 2 Aug 30 commit

--- src/Investment.java
+++ src/Investment.java
@@ -4,7 +4,8 @@ public class Investment {
         return c*Math.pow(1+r/n, t*n);
         // return sends the value back to the function and can be used later.
     }
-    public static void main(String [] args){
-        System.out.println(investment(1.0, 1.0 , 1 , (int)1e9));
+    public static void main(String [] args) {
+        System.out.println(investment(1.0, 1.0, 1, (int) 1e9));
+        System.out.println(investment(1000, 0.03, 10, 365));
     }
 }
